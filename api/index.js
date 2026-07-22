8b08cc5b5cec68d222e727efa8527abc665121db
alysson esser <alyssonrenan123456@gmail.com>
2026-07-22 09:47:16 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Conexão direta com o Turso usando protocolo libsql://
 const db = createClient({
-  url: 'libsql://logscomercial-alyssonrenan123456-jpg.aws-us-east-1.turso.io',
+  url: 'https://logscomercial-alyssonrenan123456-jpg.aws-us-east-1.turso.io',
   authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODQ3MjI4NTgsImlkIjoiMDE5Zjg5YzMtYzIwMS03ZmFmLWI1YjItZTU1ZjUzNGZjZTAzIiwia2lkIjoicDRjeVRXTlZ5YWUtMXRUSkh6S2w0ZU54bUZVRm8ySXhTaDdTRHJkRWJxQSIsInJpZCI6IjdlNmU4MzYyLTg1YzUtNDIyOC04NTAxLTE4YThlMTQzNzU4MCJ9.zje1JEqAcrMc1b-md3gVOH9L6eV1vrnAYNakSVpcoAYqH8nV43L48JzHkq843jYnRCZfZELPPeeUTyokkFt5CA',       // Cole o Token do Turso aqui
 });
 
